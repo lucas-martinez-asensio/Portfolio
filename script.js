98ae087ac1e784968d74187f825c68550d21b693
lucas-martinez-asensio <109492404+lucas-martinez-asensio@users.noreply.github.com>
2022-07-19 23:44:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector(".nav_button-container");
 const openMenuButton = document.querySelector(".open-menu");
 const closeMenuButton = document.querySelector(".close-menu");
+const sectionMenuButton = document.quertSelector(".nav_button-container--li")
 
 function toggleMenu() {
   menu.classList.toggle("menu_opened");
@@ -8,3 +9,4 @@ function toggleMenu() {
 
 openMenuButton.addEventListener("click", toggleMenu);
 closeMenuButton.addEventListener("click", toggleMenu);
+sectionMenuButton.addEventListener("click", toggleMenu);
